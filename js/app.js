5481d8094a39019f88897267efdb94caabea9135
Naoki Yamamoto <yamamoto.naoki.028@tis.co.jp>
2015-02-18 13:00:23 +0900

    日付のモック作成

--- js/app.js
+++ js/app.js
@@ -54,6 +54,44 @@ app.config(["$stateProvider", "$urlRouterProvider", "$httpProvider", "$mdTheming
 }]);
 
 app.controller('AppCtrl', ['$scope', function($scope){
-  
+
+    this.cals = [
+        { "year" : "2014", "month" : "12", "day" : "28", "weekday" : "日", "holiday" : "" },
+        { "year" : "2014", "month" : "12", "day" : "29", "weekday" : "月", "holiday" : "" },
+        { "year" : "2014", "month" : "12", "day" : "30", "weekday" : "火", "holiday" : "" },
+        { "year" : "2014", "month" : "12", "day" : "31", "weekday" : "水", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "01", "weekday" : "木", "holiday" : "元日" },
+        { "year" : "2015", "month" : "01", "day" : "02", "weekday" : "金", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "03", "weekday" : "土", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "04", "weekday" : "日", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "05", "weekday" : "月", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "06", "weekday" : "火", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "07", "weekday" : "水", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "08", "weekday" : "木", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "09", "weekday" : "金", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "10", "weekday" : "土", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "11", "weekday" : "日", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "12", "weekday" : "月", "holiday" : "成人の日" },
+        { "year" : "2015", "month" : "01", "day" : "13", "weekday" : "火", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "14", "weekday" : "水", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "15", "weekday" : "木", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "16", "weekday" : "金", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "17", "weekday" : "土", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "18", "weekday" : "日", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "19", "weekday" : "月", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "20", "weekday" : "火", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "21", "weekday" : "水", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "22", "weekday" : "木", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "23", "weekday" : "金", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "24", "weekday" : "土", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "25", "weekday" : "日", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "26", "weekday" : "月", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "27", "weekday" : "火", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "28", "weekday" : "水", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "29", "weekday" : "木", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "30", "weekday" : "金", "holiday" : "" },
+        { "year" : "2015", "month" : "01", "day" : "31", "weekday" : "土", "holiday" : "" }
+    ];
  
 }]);
+
