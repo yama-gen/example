e7fe2144076ffe78fc46335b78105c48c425d9ca
yama-gen <naoki.aoi.y@gmail.com>
2015-10-10 12:07:05 +0900

    no message

--- src/main/java/jp/co/gfam/example/ctrl/EventController.java
+++ src/main/java/jp/co/gfam/example/ctrl/EventController.java
@@ -22,10 +22,10 @@ public class EventController {
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Event> get() {
-//        eventRepository.save(new ArrayList<Event>() {{
-//            add(new Event("第5回GFAM杯", "2015/10/10(土) 08:00", "2015/10/10(土) 16:00", "取手国際ゴルフ倶楽部", 20000L, 20L, 14L));
-//            add(new Event("バーベキュー", "2015/10/24(土) 10:00", "2015/10/25(日) 10:00", "小田原", 5000L, 15L, 12L));
-//        }});
+        eventRepository.save(new ArrayList<Event>() {{
+            add(new Event("第5回GFAM杯", "2015/10/10(土) 08:00", "2015/10/10(土) 16:00", "取手国際ゴルフ倶楽部", 20000L, 20L, 14L));
+            add(new Event("バーベキュー", "2015/10/24(土) 10:00", "2015/10/25(日) 10:00", "小田原", 5000L, 15L, 12L));
+        }});
 
         return eventRepository.findAll();
     }
